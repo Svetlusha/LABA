40144814fefb2c6885870cd2a4ce086218cad441
user <user@WIN-2TKTPQ3MJIT>
2023-03-08 22:22:19 +0500

    Создала класс array

---
+++ Laba9/src/main/java/Laba2/array.java
@@ -0,0 +1,19 @@
+package Laba2;
+
+public class array {
+	public static int min = 900;
+    public static int max = 1200;
+    public int[] rr() {
+
+    	generation arr = new generation();
+
+        int[] Buff = new int[15];
+        for (int i = 0; i < 15; i++) {
+            Buff[i] = arr.randomizer(min, max);
+            
+
+
+        }
+        return Buff;
+    }
+}
