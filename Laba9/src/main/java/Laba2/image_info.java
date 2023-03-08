ed05731c2d4336036cfecdefdd0ac22fc132ce12
shahs <shahs@192.168.56.1>
2023-03-08 21:24:47 +0500

    Добавила отображение логотипа в jar файле

--- Laba9/src/main/java/Laba2/image_info.java
+++ Laba9/src/main/java/Laba2/image_info.java
@@ -4,8 +4,13 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class image_info extends JComponent {
 	private Image image;
@@ -13,8 +18,14 @@ public class image_info extends JComponent {
 	public image_info() {
 			// Загрузка изображения.
 		try {
-			image = ImageIO.read(new File("src/main/resources/Images/Логотип.png"));
-			
+			//image = ImageIO.read(new File("src/main/resources/Images/Логотип.png"));
+			Path path = Paths.get("Логотип.png");
+			BufferedImage buf = ImageIO.read(path.toFile());
+			ByteArrayOutputStream bytear = new ByteArrayOutputStream();
+	        ImageIO.write(buf, "png", bytear);
+	        byte[] bytes = bytear.toByteArray();
+	        InputStream is = new ByteArrayInputStream(bytes);
+	        image = ImageIO.read(is);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
