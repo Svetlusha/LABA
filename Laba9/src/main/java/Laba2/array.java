b6d569cc8faa32a31187ac0ded22d272284bce6e
Элина <@DESKTOP-HO948VP>
2023-03-08 22:42:11 +0500

    Создала класс, который реализует сортировку пузырьковым методом

--- Laba9/src/main/java/Laba2/array.java
+++ Laba9/src/main/java/Laba2/array.java
@@ -1,14 +1,14 @@
 package Laba2;
 
 public class array {
-	public static int min = 900;
-    public static int max = 1200;
+	public static int min = 100;
+    public static int max = 700;
     public int[] rr() {
 
     	generation arr = new generation();
 
-        int[] Buff = new int[15];
-        for (int i = 0; i < 15; i++) {
+        int[] Buff = new int[10];
+        for (int i = 0; i < 10; i++) {
             Buff[i] = arr.randomizer(min, max);
             
 
